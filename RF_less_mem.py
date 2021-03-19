15faa86dd3d32ea5e359695fab4dabd95ff0a3e7
Charles Hawkins <charles.l.hawkins@wsu.edu>
2021-03-19 15:37:53 -0700

    commit to main

--- RF_less_mem.py
+++ RF_less_mem.py
@@ -11,6 +11,8 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import log_loss
 from matplotlib import pyplot as plt
+from sklearn.model_selection import cross_val_score
+
 
 df = pd.read_csv("https://raw.githubusercontent.com/pplonski/datasets-for-start/master/adult/data.csv", 
                  skipinitialspace=True)
@@ -28,7 +30,11 @@ X = df[df.columns[:-1]]
 y = df["income"]
 
 ## Let’s use 25% of the data for testing and the rest for training.
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=56)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=56)
+
+## Let’s use 10% of the data for testing and the rest for training.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=56)
+
 rf = RandomForestClassifier()
 rf.fit(X_train, y_train)
 # print(rf.estimators_[0].tree_.max_depth)
@@ -42,22 +48,29 @@ print(f"Single tree size: {np.round(os.path.getsize('first_tree_from_RF.joblib')
 joblib.dump(rf, "RandomForest_100_trees.joblib") 
 print(f"Random Forest size: {np.round(os.path.getsize('RandomForest_100_trees.joblib') / 1024 / 1024, 2) } MB")
 
-y_predicted = rf.predict_proba(X_test)
-rf_loss = log_loss(y_test, y_predicted)
-print(rf_loss)
+print(f"Cross validation: {np.mean(cross_val_score(estimator, X))}")
+###########################
+# 10-Fold Cross validation
+print np.mean(cross_val_score(rf, X_train, y_train, cv=10))
 
-shallow_rf = RandomForestClassifier(max_depth=6)
-shallow_rf.fit(X_train, y_train)
+###########################
 
-joblib.dump(shallow_rf.estimators_[0], "first_tree_from_shallow_RF.joblib") 
-print(f"Single tree size from shallow RF: {np.round(os.path.getsize('first_tree_from_shallow_RF.joblib') / 1024 / 1024, 2) } MB") # this line is the same as 38
+# y_predicted = rf.predict_proba(X_test)
+# rf_loss = log_loss(y_test, y_predicted)
+# print(rf_loss)
 
-joblib.dump(shallow_rf, "Shallow_RandomForest_100_trees.joblib") 
-print(f"Shallow Random Forest size: {np.round(os.path.getsize('Shallow_RandomForest_100_trees.joblib') / 1024 / 1024, 2) } MB")
+# shallow_rf = RandomForestClassifier(max_depth=6)
+# shallow_rf.fit(X_train, y_train)
 
-y_predicted = shallow_rf.predict_proba(X_test)
-shallow_rf_loss = log_loss(y_test, y_predicted)
-print(shallow_rf_loss)
+# joblib.dump(shallow_rf.estimators_[0], "first_tree_from_shallow_RF.joblib") 
+# # print(f"Single tree size from shallow RF: {np.round(os.path.getsize('first_tree_from_shallow_RF.joblib') / 1024 / 1024, 2) } MB") # this line is the same as 38
 
-joblib.dump(rf, "RF_compressed.joblib", compress=3)
-print(f"Compressed Random Forest: {np.round(os.path.getsize('RF_compressed.joblib') / 1024 / 1024, 2) } MB")
+# joblib.dump(shallow_rf, "Shallow_RandomForest_100_trees.joblib") 
+# print(f"Shallow Random Forest size: {np.round(os.path.getsize('Shallow_RandomForest_100_trees.joblib') / 1024 / 1024, 2) } MB")
+
+# y_predicted = shallow_rf.predict_proba(X_test)
+# shallow_rf_loss = log_loss(y_test, y_predicted)
+# print(shallow_rf_loss)
+
+# joblib.dump(rf, "RF_compressed.joblib", compress=3)
+# # print(f"Compressed Random Forest: {np.round(os.path.getsize('RF_compressed.joblib') / 1024 / 1024, 2) } MB")
